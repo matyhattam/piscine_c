a0eb4bfaa0710da523793106a4bab0360d759df4
maty <maty.hattam@gmail.com>
2026-02-23 13:19:20 +0100

    cleaning

--- c11/ft_is_sort.c
+++ c11/ft_is_sort.c
@@ -1,22 +1,5 @@
 #include <stdio.h>
 
-void ft_sort_int_tab(int *tab, int size) {
-  int i = 0;
-  while (i < size) {
-    int j = 0;
-    while (j < size) {
-      if (tab[j] > tab[j + 1]) {
-        int temp;
-        temp = tab[j];
-        tab[j] = tab[j + 1];
-        tab[j + 1] = temp;
-      }
-      j++;
-    }
-    i++;
-  }
-}
-
 int ft_check(int a, int b) {
   if (a > b) {
     return -1;
